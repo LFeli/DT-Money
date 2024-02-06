5e6c45d6d253ea8944feade6b9051c64122eab1a
Lucas Felipe <lucasfelipemk@gmail.com>
2024-02-06 20:01:42 -0300

    :lipstick: style: Add font and initial style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <main className="h-screen w-screen bg-zinc-950 text-white flex items-center justify-center">
+      <h1 className="text-8xl font-bold">Hello World</h1>
+    </main>
   )
 }
 
