8ffe2d0877809a49af117163c5a0c0b43b81e735
Lucas Felipe <lucasfelipemk@gmail.com>
2024-02-13 14:47:48 -0300

    :sparkles: feat: First request in react usign http

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
 import { PriceHighlight } from './components/PriceHighlight'
 import { SearchForm } from './components/SearchForm'
 import { Summary } from './components/Summary'
 
+interface Transactions {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
 export function App() {
+  const [transactions, setTransactions] = useState<Transactions[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <main className="h-full text-gray-100">
       <Header />
@@ -13,28 +36,24 @@ export function App() {
         <SearchForm />
         <table className="w-full mt-6 border-separate border-spacing-y-2">
           <tbody>
-            <tr>
-              <td className="px-8 py-5 bg-gray-700 w-1/2 rounded-tl-md rounded-bl-md">
-                Desenvolvimento de site
-              </td>
-              <PriceHighlight statusColor="income">R$ 10.000,00</PriceHighlight>
-              <td className="px-8 py-5 bg-gray-700 ">Venda</td>
-              <td className="px-8 py-5 bg-gray-700 rounded-tr-md rounded-br-md">
-                13/04/2022
-              </td>
-            </tr>
-            <tr>
-              <td className="px-8 py-5 bg-gray-700 w-1/2 rounded-tl-md rounded-bl-md">
-                Aluguel do apartamento
-              </td>
-              <PriceHighlight statusColor="outcome">
-                - R$ 1.200,00
-              </PriceHighlight>
-              <td className="px-8 py-5 bg-gray-700 ">Casa</td>
-              <td className="px-8 py-5 bg-gray-700 rounded-tr-md rounded-br-md">
-                13/04/2022
-              </td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td className="px-8 py-5 bg-gray-700 w-1/2 rounded-tl-md rounded-bl-md">
+                    {transaction.description}
+                  </td>
+                  <PriceHighlight statusColor={transaction.type}>
+                    {transaction.price}
+                  </PriceHighlight>
+                  <td className="px-8 py-5 bg-gray-700 ">
+                    {transaction.category}
+                  </td>
+                  <td className="px-8 py-5 bg-gray-700 rounded-tr-md rounded-br-md">
+                    {transaction.createdAt}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
